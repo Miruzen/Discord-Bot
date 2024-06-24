c1032aeb6b3d4bee9a551f612ecbfcd011f07ae2
Miruzen <nandagouwputra@gmail.com>
2024-06-24 11:01:21 +0700

    3rd data training

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def send_message(message, user_message, is_private):
 
 def run_bot():
     """Runs the Discord bot."""
-    TOKEN = "Enter your discord Token "
+    TOKEN = "Enter your discord Token" 
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
